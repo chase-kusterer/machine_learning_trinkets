601b7ade9c3325c6a414a1fca817e4b4eadd4113
Chase Kusterer <50032310+chase-kusterer@users.noreply.github.com>
2020-01-03 17:21:33 -0800

    Update plot_feature_importances.py

--- plot_feature_importances.py
+++ plot_feature_importances.py
@@ -1,10 +1,12 @@
-import pandas            as pd                       # data science essentials
+# required packages
 import matplotlib.pyplot as plt                      # data visualization
+
 
 # setting X_train to None so the function loads properly
 X_train = None
 
 
+# plot_feature_importances
 def plot_feature_importances(model, train = X_train, export = False):
     """
     Plots the importance of features from a CART model.
